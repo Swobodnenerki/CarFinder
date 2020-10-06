2b501f3fac66f1c96e12736d177fec0a8172af3b
Swobodnenerki <61480793+Swobodnenerki@users.noreply.github.com>
2020-10-07 00:23:58 +0200

    Security configuration

--- src/main/java/org/kozak/carfinder/Models/UserDetailsImplementation.java
+++ src/main/java/org/kozak/carfinder/Models/UserDetailsImplementation.java
@@ -1,4 +1,71 @@
 package org.kozak.carfinder.Models;
 
-public class UserDetailsImplementation {
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+public class UserDetailsImplementation implements UserDetails {
+    private String login;
+    private String password;
+    private boolean enabled;
+    private SimpleGrantedAuthority authority;
+    private AccountEntity accountEntity;
+
+    public UserDetailsImplementation(AccountEntity account, String role) {
+
+        this.login = account.getLogin();
+        this.password = account.getPassword();
+        if(role.equals("USER"))
+            enabled=false;
+        else
+            enabled=true;
+
+        this.authority = new SimpleGrantedAuthority(role);
+        this.accountEntity = account;
+    }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Arrays.asList(authority);
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public AccountEntity getUserAccountsEntity() {
+        return accountEntity;
+    }
+
+    public void setUserAccountsEntity(AccountEntity accountEntity) {
+        this.accountEntity = accountEntity;
+    }
 }
--- src/main/java/org/kozak/carfinder/Services/Implementation/UserDetailsServiceImplementation.java
+++ src/main/java/org/kozak/carfinder/Services/Implementation/UserDetailsServiceImplementation.java
@@ -1,6 +1,51 @@
 package org.kozak.carfinder.Services.Implementation;
 
+import org.kozak.carfinder.Models.AccountEntity;
+import org.kozak.carfinder.Models.UsersEntity;
+import org.kozak.carfinder.Models.RolesEntity;
+import org.kozak.carfinder.Models.UserDetailsImplementation;
+import org.kozak.carfinder.Models.UsersEntity;
+import org.kozak.carfinder.Repositories.API.IRolesDao;
+import org.kozak.carfinder.Repositories.API.IAccountDao;
+import org.kozak.carfinder.Repositories.API.IUsersDao;
+import org.kozak.carfinder.Repositories.API.IDealerDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
+public class UserDetailsServiceImplementation implements UserDetailsService {
 
-public class UserDetailsServiceImplementation {
+    @Autowired
+    IUsersDao userDao;
+
+    @Autowired
+    IAccountDao accountDao;
+
+    @Autowired
+    IDealerDao dealerDao;
+
+    @Autowired
+    IRolesDao rolesDao;
+
+    @Override
+    public UserDetails loadUserByUsername(String userLogin) throws UsernameNotFoundException {
+        AccountEntity account = accountDao.findAccountEntitiesByLogin(userLogin);
+        UsersEntity user = userDao.findUsersEntitiesByAccountByAccountid(account);
+        RolesEntity role = rolesDao.findRolesEntitiesByAccountByAccountid(account);
+        if (account == null) {
+            throw new UsernameNotFoundException("There is no user with login: " + userLogin);
+        }
+//        if(dealerDao.findDealerEntitiesByUsersByUserid(user)==null) {
+//            return new UserDetailsImplementation(account, "ROLE_DEALER");
+//        }
+        if(role.getRole()=="dealer"){
+            return new UserDetailsImplementation(account, "DEALER");
+        }
+        else {
+            return new UserDetailsImplementation(account, "USER");
+        }
+    }
 }
